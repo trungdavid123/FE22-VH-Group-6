543309e0e619810f3d6c1eb784cc84696a5ef582
David <trungdavid969@gmail.com>
2023-01-18 21:46:58 +0100

    add files

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 const hamburgerBtn = document.querySelector('.menu-hamburgare img');
 const linkList = document.querySelector('.link-list');
+const contactUsContainer = document.querySelector('.contact-us');
+const header = document.querySelector('.header');
+
+
 
 hamburgerBtn.addEventListener('click', () => {
     linkList.classList.toggle('show');
+    contactUsContainer.classList.toggle('show');
+    header.classList.toggle('show');
+
 })
